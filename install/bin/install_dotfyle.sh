db528226cec6b3264e38c8604f996d34b06c72de
Your Name <you@example.com>
2024-09-20 23:21:42 +0200

    aider: chore: reorganize configuration files and remove obsolete installation scripts

--- install/bin/install_dotfyle.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Download the dotfyle script to the local bin directory
-curl -o ~/.local/bin/dotfyle https://raw.githubusercontent.com/RoryNesbitt/dotfyle-cli/main/dotfyle
-
-# Make the script executable
-chmod +x ~/.local/bin/dotfyle
-
-echo "dotfyle has been installed successfully."
--- stow/bin/dump.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Set Git user name and email
-echo "[skogix]: Setting Git user name"
-git config --global user.name "Emil Skogsund"
-echo "[skogix]: Setting Git user email"
-git config --global user.email "emil.skogsund@gmail.com"
