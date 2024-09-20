cda6792e22781dff0c54878b2f9ff5d7cfc3753b
Your Name <you@example.com>
2024-09-20 19:47:16 +0200

    skogix: remove files

--- stow/bin/dump.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Set Git user name and email
-git config --global user.name "Emil Skogsund"
-git config --global user.email "emil.skogsund@gmail.com"
--- stow/bin/zsh.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/zsh
-
-ln -s "$HOME/.config/zsh/zshrc" "$HOME/.zshrc"
