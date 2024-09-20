48ddb1cfd4ce1fe6cdc642803c51c58516166274
Your Name <you@example.com>
2024-09-20 20:46:29 +0200

    aider: fix: remove argument check and simplify stow_folder.sh script

--- stow/bin/stow_folder.sh
+++ stow/bin/stow_folder.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
 
-# Check if the correct number of arguments is provided
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <folder-name>"
-    exit 1
-fi
-
-# Get the folder name from the argument
-FOLDER_NAME=$1
-
 # Change to the dotfiles directory
 cd ~/dot || exit
-
-# Run stow on the specified folder
-stow "$FOLDER_NAME"
